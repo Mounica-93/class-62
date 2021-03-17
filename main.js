b88de5d6104de31f5d22c0a79369a8322c63eb68
Mounica-93 <78643750+Mounica-93@users.noreply.github.com>
2021-03-17 12:43:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function cp(){
+    document.getElementById("ad1").play();
+    document.getElementById("img1").src="cat2.jpg";}
+
+
+function changeimg(){
+    document.getElementById("ad1").play();
+    document.getElementsById("img2").src="dog2.jpg";}
+    
